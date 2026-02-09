151b9360cc94d9c59cd7db38ef19683f999715bf
Aminul Islam <aminalislam4223@gmail.com>
2026-02-09 13:14:47 +0600

    react helmet added

--- book-vibe/src/pages/Books/Books.jsx
+++ book-vibe/src/pages/Books/Books.jsx
@@ -1,25 +1,34 @@
-import { useLoaderData } from 'react-router'
-import Book from '../Book/Book'
-import { useState } from 'react'
+import Book from "../Book/Book";
+import { useState } from "react";
+import { Helmet } from "react-helmet-async";
 
-export default function Books({books}) {
+export default function Books({ books, showHelmet = true }) {
   const [sortedBooks, setSortedBooks] = useState([]);
   const handleSort = () => {
-    const sortedList = [...books].sort((a,b)=>a.pages - b.pages);
+    const sortedList = [...books].sort((a, b) => a.pages - b.pages);
     setSortedBooks(sortedList);
-  }
+  };
 
   return (
-    <div className='flex flex-col justify-center items-center m-5'>
-      <p className='text-2xl text-center mb-2'>Available Books</p>
-      <div className='flex justify-start w-full mb-5'> 
-        <button onClick={handleSort} className='btn btn-primary mb-5'>Sort</button>
+    <div className="flex flex-col justify-center items-center m-5">
+      {showHelmet && (
+        <Helmet>
+          <title>Books | Book Vibe</title>
+        </Helmet>
+      )}
+      <p className="text-2xl text-center mb-2">Available Books</p>
+      <div className="flex justify-start w-full mb-5">
+        <button onClick={handleSort} className="btn btn-primary mb-5">
+          Sort
+        </button>
       </div>
-     <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
-       {
-        sortedBooks.length > 0 ? sortedBooks.map(book => <Book key={book.isbn_13} book={book}></Book>) : books.map(book => <Book key={book.isbn_13} book={book}></Book>)
-      }
-     </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {sortedBooks.length > 0
+          ? sortedBooks.map((book) => (
+              <Book key={book.isbn_13} book={book}></Book>
+            ))
+          : books.map((book) => <Book key={book.isbn_13} book={book}></Book>)}
+      </div>
     </div>
-  )
+  );
 }
--- book-vibe/src/pages/ErrorPage/Error.jsx
+++ book-vibe/src/pages/ErrorPage/Error.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Helmet } from "react-helmet-async";
 
 export default function Error() {
   return (
     <div>
-      <h1 className='flex justify-center items-center'>404 Not Found</h1>
+      <Helmet>
+        <title>404 | Book Vibe</title>
+      </Helmet>
+      <h1 className="flex justify-center items-center">404 Not Found</h1>
     </div>
-  )
+  );
 }
